fd3d011dfe61dca42280f8cdc6fe9301e3ec72fa
hectordelangel <h-delangel04@hotmail.com>
2021-12-16 23:17:25 -0800

    Mood selector finished

---
+++ lib/Models/mood_selector.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class MoodSelectorModel {
+  String img;
+  String mood;
+
+  MoodSelectorModel({
+    required this.img,
+    required this.mood,
+  });
+}
--- lib/moodamay_entry.dart
+++ lib/moodamay_entry.dart
@@ -1,5 +1,8 @@
+import 'dart:math';
+
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:moodamay/Models/mood_selector.dart';
 import '../constant.dart';
 
 class Mood extends StatefulWidget {
@@ -10,24 +13,73 @@ class Mood extends StatefulWidget {
 }
 
 class _MoodState extends State<Mood> {
+  double _currentSliderValue = 3;
   @override
   void initState() {
     super.initState();
   }
+
+  List<MoodSelectorModel> moods = <MoodSelectorModel>[
+    MoodSelectorModel(img: 'assets/images/angry.gif', mood: 'Angry'),
+    MoodSelectorModel(img: 'assets/images/sad.gif', mood: 'Sad'),
+    MoodSelectorModel(img: 'assets/images/annoyed.gif', mood: 'Annoyed'),
+    MoodSelectorModel(img: 'assets/images/happy.gif', mood: 'Happy'),
+    MoodSelectorModel(img: 'assets/images/surprised.gif', mood: 'Surprised'),
+    MoodSelectorModel(img: 'assets/images/silly.gif', mood: 'Silly'),
+    MoodSelectorModel(img: 'assets/images/flirty.gif', mood: 'Flirty')
+  ];
 
   @override
   Widget build(BuildContext context) {
     return SafeArea(
       top: false,
       child: Scaffold(
-          appBar: AppBar(),
+          appBar: AppBar(
+            backgroundColor: kblue,
+            foregroundColor: babyblue,
+            title: Text(
+              'Select your mood!',
+              style: TextStyle(fontSize: 25),
+            ),
+          ),
           body: Container(
             color: white,
             child:
-                Column(mainAxisAlignment: MainAxisAlignment.center, children: [
+                Column(mainAxisAlignment: MainAxisAlignment.start, children: [
               Container(
                 alignment: Alignment.topCenter,
-                child: Text("Puto Mood"),
+                child: Padding(
+                  padding: const EdgeInsets.all(20),
+                  child: Column(
+                    children: [
+                      Image.asset(
+                        moods[_currentSliderValue.toInt()].img,
+                        height: MediaQuery.of(context).size.height * .35,
+                      ),
+                      Text(moods[_currentSliderValue.toInt()].mood,
+                          textAlign: TextAlign.center,
+                          style: TextStyle(
+                            fontSize: 25.0,
+                            fontWeight: FontWeight.bold,
+                            fontFamily: 'Poppins',
+                            color: babyblue,
+                          )),
+                      Slider(
+                        value: _currentSliderValue,
+                        thumbColor: kblue,
+                        inactiveColor: Color.fromRGBO(205, 180, 219, .5),
+                        activeColor: kblue,
+                        max: 6,
+                        onChanged: (double value) {
+                          setState(() {
+                            _currentSliderValue = value;
+                            debugPrint(_currentSliderValue.toString());
+                          });
+                        },
+                      )
+                    ],
+                  ),
+                ),
               )
             ]),
           )),
